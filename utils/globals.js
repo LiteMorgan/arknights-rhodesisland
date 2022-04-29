65d2eea3e3561358db2131d662e70564d1af1b2e
getignited <liam@liammorgan.dev>
2022-04-29 15:54:01 +0100

    Add global options

---
+++ utils/globals.js
@@ -0,0 +1,21 @@
+export const PROFESSION_SORT_ORDER = [
+  "WARRIOR",
+  "SNIPER",
+  "TANK",
+  "MEDIC",
+  "SUPPORT",
+  "CASTER",
+  "SPECIAL",
+  "PIONEER",
+];
+
+export const PROFESSION_REFERENCE = {
+  WARRIOR: "Guard",
+  SNIPER: "Sniper",
+  TANK: "Defender",
+  MEDIC: "Medic",
+  SUPPORT: "Supporter",
+  SPECIAL: "Specialist",
+  CASTER: "Caster",
+  PIONEER: "Vanguard",
+};
