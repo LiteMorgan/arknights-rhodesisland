72c51a90db98d01c76abf33c1a504fd23b8b47e2
getignited <liam@liammorgan.dev>
2022-11-01 22:10:40 +0000

    update Next files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,48 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const path = require("path");
 
-module.exports = nextConfig;
+module.exports = async (basePath, phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    redirects: () => {
+      return [
+        {
+          source: "/operators",
+          destination: "/operators/recruitment",
+          permanent: true,
+        },
+      ];
+    },
+    webpack: (config, options) => {
+      config.module.rules.push(
+        {
+          include: [
+            path.resolve(basePath, "/"),
+            path.resolve(__dirname, "./components/icons"),
+          ],
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        },
+        {
+          test: /\.yml$/,
+          use: [
+            { loader: require.resolve("json-loader") },
+            {
+              loader: require.resolve("yaml-loader"),
+              options: {
+                asJSON: true,
+              },
+            },
+          ],
+        }
+      );
+      config.resolve.extensions.push(".yml");
+
+      return config;
+    },
+  };
+  return nextConfig;
+};
