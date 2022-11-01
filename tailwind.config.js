2fcd8aafd633cba8433818fd08a3a2704f0e8137
getignited <liam@liammorgan.dev>
2022-11-01 22:09:50 +0000

    update Tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./context/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./scripts/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         stars: "#fbd631",
+        rarity1: "#ffffff",
+        rarity2: "#dfe55b",
+        rarity3: "#50b0f7",
+        rarity4: "#dbb1db",
       },
     },
   },
